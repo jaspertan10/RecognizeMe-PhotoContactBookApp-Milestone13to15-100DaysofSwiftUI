b446e8b5eebf6c0b3191f108c951e40f5970e943
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2025-02-14 18:41:38 -0800

    Initial repo

---
+++ RecognizeMe/RecognizeMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecognizeMeApp.swift
+//  RecognizeMe
+//
+//  Created by Jasper Tan on 2/14/25.
+//
+
+import SwiftUI
+
+@main
+struct RecognizeMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
